5e36255a783e58208ebcf7e0e4192f3f7c5351be
Saavi Verma <saaviverma2006@gmail.com>
2025-04-28 11:34:03 +0530

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,7 +1,6 @@
 document.querySelectorAll('.faq-question').forEach(button => {
     button.addEventListener('click', () => {
         const faqItem = button.parentElement;
-        
         // Toggle Active State
         faqItem.classList.toggle('active');
     });
